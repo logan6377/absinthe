7538b91a2024385adc388023a526150d57f85812
Logan <loganathan.arumugam@borngroup.com>
2018-02-06 05:09:26 +0530

    Pipe issue fixed for new data

--- src/app/services/bucket.pipe.ts
+++ src/app/services/bucket.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Task } from '../task';
 
 @Pipe({
-  name: 'bucket'
+  name: 'bucket',
+  pure: false
 })
 export class BucketPipe implements PipeTransform {
   task:Task[]=[];
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit   } from '@angular/core';
 import { TaskDetailsService } from '../services/task-details.service';
 import { Task } from '../task';  
 import { Tasklist } from '../services/tasklist';
@@ -8,26 +8,38 @@ import { Tasklist } from '../services/tasklist';
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent implements OnInit {
-  hdata
-  taskdata  
+export class TaskListComponent implements OnInit { 
+  taskdata; 
   
-  constructor(private http:TaskDetailsService) { }
+  constructor(private http:TaskDetailsService ) { 
+    this.taskdata = this.http.getCurrentTask(); 
+   }
 
   ngOnInit() {
-    this.taskdata = this.http.getCurrentTask(); 
+    
   } 
 
   addNewTask(){ 
-    let newtask = new Task();
-    newtask.CONVERGE_ID = "ABCD" 
-
-    this.http.addNewTask(newtask)  
-  }
-
-  getHabitica(){
-    this.hdata = this.http.habitica();
-    console.log('aaa----',this.hdata)
-  }
+    this.http.addNewTask({
+      TASK_ID:123456,
+      CONVERGE_ID:'01_555_ABCD_123456',
+      JOBTYPE :'New Build',
+      COMPLEXITY:'Simple',
+      SCHEDULED_START_DATE:new Date("2016-01-16T16:00:00"),
+      SCHEDULED_END_DATE:new Date("2016-01-16T16:00:00"),
+      STATUS:'InProgress',
+      PUBLISHER:'WM',
+      POCS:'Bill Edgington',
+      JOB_COMMENTS:'Sample job description',
+      SCHEDULED_HOURS:32,
+      TOTAL_NO_OF_FUNCTIONALITIES:10,
+      FUNCTIONALITY_DETAILS:'Sample functionality description',
+      EDIT_REASON:'Sample edit description',
+      ACTUAL_START_TIME:new Date("2016-01-16T16:00:00"),
+      ACTUAL_END_TIME:new Date("2016-01-16T16:00:00"),
+      ACTUAL_HOURS:16,TOKEN_ID:'ABCDEF123456'
+    });      
+  }  
+  
 
 }

aac2b8ef7271d469c96f5a5bae916d9ecadebf52
Anandharaj Venkatesan <anandharaj.v@wearefmg.net>
2018-03-03 00:57:16 +0530

    'test'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
 import { AngularFireModule } from 'angularfire2'; 
 import { HttpModule } from '@angular/http';
 import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
